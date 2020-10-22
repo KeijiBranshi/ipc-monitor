15650a20a65159d8d64a60b03d26645a916489ea
Alexey Kuzmin <alex.s.kuzmin@gmail.com>
2020-10-22 20:41:40 +0300

    fix: require main or renderer parts only when needed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import "rxjs/add/operator/mergeAll";
 import { Observable } from "rxjs/Observable";
+import { from } from "rxjs/observable/from";
 import { IpcMark } from "./common/types";
 
-import mainProcessMonitor from "./main";
-import rendererProcessMonitor from "./renderer";
-
-const ipcMonitor: Observable<IpcMark> =
-  process?.type === "renderer" ? rendererProcessMonitor : mainProcessMonitor;
+const ipcMonitor: Observable<IpcMark> = from(
+  process?.type === "renderer"
+    ? import("./renderer").then((module) => module.default)
+    : import("./main").then((module) => module.default)
+).mergeAll();
 
 export { IpcMark, IpcMonitor, IpcMetric } from "./common/types";
 export default ipcMonitor;
